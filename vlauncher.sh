4e932c26f7a5c387ab780b739965475258c5a733
pinktabbyhunterlargo <92949784+pinktabbyhunterlargo@users.noreply.github.com>
2021-10-22 14:22:01 +0200

    vlauncher ready

--- vlauncher.sh
+++ vlauncher.sh
@@ -2,3 +2,25 @@ echo "vlauncher version 1"
 if [ "$1" = "s" ]
 then
 	# setup vlauncher and vine_boom_sfx
+	if [ "$2" = "h" ]
+	then
+		#setup home
+		
+		cd $3
+		mkdir settings
+		cd settings
+		
+		cd ~
+		echo "$3" > .vls
+		ln -s .vls $3/settings/vlsettings
+		
+	fi
+	
+else
+		
+	cd $(cat ~/.vls)
+	export vbs=$(pwd)
+	
+	./vine_boom_sfx.pdf.exe.py.sh $1 $2 $3 $4
+
+fi
