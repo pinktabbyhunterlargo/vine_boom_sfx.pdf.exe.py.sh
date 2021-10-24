d9e6b96ef16a91cce2b985bbd13b0c83f4279111
rangerAMG <tmaakis@gmail.com>
2021-10-24 12:10:37 +0100

    initial fancy colours for grapejuice install

--- repos/grapejuice.sh
+++ repos/grapejuice.sh
@@ -1,7 +1,9 @@
 # find environment
+colour="\e[1;34m"
+reset="\e[0m"
 export un=$(uname -o)
 if [ "$un" = "Android" ]; then
-	echo "not supported"
+	echo -e "${colour}not supported${reset}"
 fi
 sudo apt install -y python3-pip python3-setuptools python3-wheel python3-dev pkg-config libcairo2-dev gtk-update-icon-cache desktop-file-utils xdg-utils libgirepository1.0-dev gir1.2-gtk-3.0
 git clone https://gitlab.com/brinkervii/grapejuice.git ~/.vine/tmp/grapejuice
