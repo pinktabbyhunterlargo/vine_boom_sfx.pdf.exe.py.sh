f333784e287d662d1fdf8134d22655004e8a079e
pinktabbyhunterlargo <92949784+pinktabbyhunterlargo@users.noreply.github.com>
2021-11-19 22:58:01 +0100

    Add files via upload

---
+++ bpm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+#	we got bloat les gooo
+
+def install()
+	"""Python-based installer capable of multithreading."""
+inputs = sys.argv[1:]
+ityp = inputs[0]
+
+if ityp == "install":
+	inputs[0] = []
+	
+	#	time to check for --flags
+	#	trolled
+	
+for i in range(len(inputs)
+	
+	subprocess.run(['bash', '../repos/bpm-call.sh', inputs[i]])
+
+	
